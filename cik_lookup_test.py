f6f105b41d174f073a599414afba16b399c5b982
GorensteinAY <wipers_styli7n@icloud.com>
2025-02-17 11:56:06 +0100

    Unit test for CIK lookup

---
+++ cik_lookup_test.py
@@ -0,0 +1,13 @@
+import cik_lookup
+import unittest
+
+class Test_TestCIKLookup(unittest.TestCase):
+    def test_cik(self):
+        self.assertEqual(cik_lookup.get_cik_from_ticker("AAPL"),"0000320193") # Correct ticker & CIK for AAPL
+        self.assertEqual(cik_lookup.get_cik_from_ticker("TSLA"),"0001318605") # Correct ticker & CIK for TSLA
+
+        self.assertEqual(cik_lookup.get_cik_from_ticker("APPL"),"0000320193") # Incorrect ticker for AAPL
+
+
+if __name__ == '__main__':
+    unittest.main()
